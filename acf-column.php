7a3acefd0b5b5d17aad49270ff8a29b932d46177
Thomas <t.meyer@me.com>
2014-09-21 22:30:24 +0200

    New version number 1.0.1

--- acf-column.php
+++ acf-column.php
@@ -3,7 +3,7 @@
 Plugin Name: Advanced Custom Fields: Column
 Plugin URI: www.dreihochzwo.de
 Description: Adds an Advanced Custom Field field to arrange ACF fields into columns. This plugin needs the installation/activation of Advanced Custom Fields V5.
-Version: 1.1.1
+Version: 1.0.1
 Author: Thomas Meyer
 Author URI: www.dreihochzwo.de
 License: GPLv2 or later
